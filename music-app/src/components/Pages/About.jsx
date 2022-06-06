878bf52a64c4bc1950029fa34d276a59afb8ea89
NurlanMamedzade <nurlanmemmedzade753@gmail.com>
2022-06-06 15:49:49 +0400

    last version

--- music-app/src/components/Pages/About.jsx
+++ music-app/src/components/Pages/About.jsx
@@ -8,12 +8,6 @@ const About = () => {
     return (
         <Container>
             <div className={"About"}>
-                {/*<div className="Dark-mode">
-                    <h5>Dark mode to be available soon</h5>
-                    <IconButton>
-                        <Brightness3/>
-                    </IconButton>
-                </div>*/}
                 <Developer/>
                 <Attribution/>
             </div>
